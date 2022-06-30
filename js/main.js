58f2cc8c7265162fa470cd0f0f5920671b00e495
Ryusuke-Kinoshita <amor.gratuitus6@gmail.com>
2022-06-30 17:52:28 +0900

    0630 home
    今からblogページ３種類作るのでいったん保存

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
-$(function(){
-    var images = $('#images');
-
-    function open(){
-        images.fadeIn(3000, 'easeInQuad', function(){ setTimeout(close, 3000); });
-    };
-    function close(){
-        images.fadeOut(1500, 'easeOutQuad', function(){ setTimeout(open, 150); });
-    };
-
-    open();
-});
+$("#images").animate({ opacity: "0" }, 3000, "linear");
+$(".image2").show({ opacity: "1" }, 3000, "linear");
